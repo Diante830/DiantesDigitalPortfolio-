f4b6e7692641276cd19f1954bd3929a050ad15dc
Diante830 <100953671@georgebrown.ca>
2024-12-09 21:19:27 +0000

    made responsive changes

--- services.js
+++ services.js
@@ -13,6 +13,21 @@ function toggleDarkMode() {
 
 let mybutton = document.getElementById("myBtn");
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function hideNav() {
   $(".main-navigation-scroll").removeClass("is-visible").addClass("is-hidden");
 }
@@ -38,3 +53,5 @@ window.onload = (event) => {
   scrollRightBtn.addEventListener("click", () => scrollRight());
   scrollLeftBtn.addEventListener("click", () => scrollLeft());
 };
+
+
